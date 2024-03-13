7a26a136a5389f5700aba952e8f114857b77c0bd
ENG Abdala Mire <121616854+AbdalaMire@users.noreply.github.com>
2024-03-13 15:31:06 -0700

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,18 @@
+const userProfile = {
+    name: 'Abdala',
+    age: 19,
+    email: 'abdalamire2843@gmail.com',
+    BookInventory: {
+        title: 'Do Less',
+        author: 'Me',
+        pages: 78,
+        available: true
+    }
+};
+
+const BookInventory = {
+    title: 'Do Less',
+        author: 'Me',
+        pages: 78,
+        available: true
+};
